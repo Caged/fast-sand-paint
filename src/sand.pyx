33629206bf2c2bfd80c587bf371669f7835aed84
Caged <justin@labratrevenge.com>
2017-07-01 21:08:45 -0700

    some absurd scale for verification

--- src/sand.pyx
+++ src/sand.pyx
@@ -54,7 +54,7 @@ cdef class Sand:
         self.h
         )
     self.ctx = cairo.Context(self.sur)
-    self.ctx.scale(0.5)
+    self.ctx.scale(5.0)
     self.set_bg([1,1,1,1])
 
   def __cinit__(self, int s):
